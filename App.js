c667b44ea792cb7f193d19e72714ba6c3cf23cc5
Felipe Marcon <felipeemarcon@gmail.com>
2018-06-23 17:15:14 -0300

    Modal, adicionar novo repositorio, AsyncStorage, componentDidMount

--- App.js
+++ App.js
@@ -5,49 +5,107 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+import { 
+  Platform, 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  View,
+  TouchableOpacity,
+  AsyncStorage,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Repo from './components/Repo';
+import NewRepoModal from './components/NewRepoModal';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  state = {
+
+    modalVisible: false,
+    repos: [],
+  };
+
+  async componentDidMount() {
+    const repos = JSON.parse(await AsyncStorage.getItem('@Reposreact:repositories')) || [];
+
+    this.setState({ repos });
+  }
+
+  _addReposity = async (newRepoText) => {
+    const repoCall = await fetch(`http://api.github.com/repos/${newRepoText}`);
+    const response = await repoCall.json();
+
+    const repository = {
+      id: response.id,
+      thumbnail: response.owner.avatar_url,
+      title: response.name,
+      author: response.owner.login,
+    };
+
+    this.setState({
+      modalVisible: false,
+      repos: [
+        ...this.state.repos,
+        repository
+      ],
+    });
+
+    await AsyncStorage.setItem('@Reposreact:repositories', JSON.stringify(this.state.repos));
+
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Minicurso GoNative</Text>
+          <TouchableOpacity onPress={() => this.setState({ modalVisible: true })}>
+            <Text style={styles.headerButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView contentContainerStyle={styles.repoList}>
+          { this.state.repos.map(repo => <Repo key={repo.id} data={repo}/>) }
+        </ScrollView>
+
+        <NewRepoModal 
+          onCancel={() => this.setState({ modalVisible: false })}  
+          onAdd ={this._addReposity}
+          visible={this.state.modalVisible}
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#333',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#e5e5e5',
+
+  header: {
+    height: (Platform.OS === 'ios') ? 70 : 50,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    backgroundColor: '#FFF',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  headerButton: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  repoList: {
+    padding: 20,
   },
 });
